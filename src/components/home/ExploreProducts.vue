35184e48023c329d1aff1197a427f6f598f187a9
Salma Waleed <salmawaleed2030@outlook.com>
2026-04-12 12:23:37 +0200

    [feat]:[ExploreProducts]: implemented explore products section

---
+++ src/components/home/ExploreProducts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="explore-products">
+    <div class="explore-products__header">
+      <SectionHeader subtitle="Our Products" title="Explore Our Products" />
+    </div>
+    <ProductList :products="products" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExploreProducts',
+    components: {
+        SectionHeader: () => import("@/components/common/SectionHeader.vue"),
+        ProductList: () => import("@/components/product/ProductList.vue"),
+    },
+  props: {
+    products: {
+      type: Array,
+      required: true,
+    },
+  }
+}
+</script>
