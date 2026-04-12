a9874d26537c6076831590e4bce14089cdc92137
Salma Waleed <salmawaleed2030@outlook.com>
2026-04-13 00:26:58 +0200

    [feat]:[404View]: implemented 404 view

---
+++ src/views/404View.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="not-found-page">
+    <h1 class="not-found-page__title">404 Not Found</h1>
+    <p class="not-found-page__message">Your visited page not found. You may go home page.</p>
+    <BaseButton @click="$router.push('/')" variant="primary">Back to home page</BaseButton>
+  </div>
+</template>
+<script>
+import BaseButton from "@/components/ui/BaseButton.vue";
+
+export default {
+  name: "NotFoundView",
+    components: {
+        BaseButton,
+    },
+};
+</script>
+<style lang="scss" scoped>
+.not-found-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    padding: 40px;
+    
+    &__title {
+        font-size: 110px;
+        font-weight: 500;
+        color: #000;
+        margin: 0;
+    }
+    
+    &__message {
+        font-size: 16px;
+        font-weight: 400;
+        margin-bottom: 30px;
+    }
+
+    @media(max-width: 768px) {
+        &__title {
+            font-size: 60px;
+        }
+    }
+
+}
+</style>
