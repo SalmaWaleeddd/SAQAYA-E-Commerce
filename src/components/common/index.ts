0aab1bf8a409df5fc01f080fb62b668fc3d418b6
Salma Waleed <salmawaleed2030@outlook.com>
2026-04-12 12:36:37 +0200

    [chore]: add barrel files in each folder to simplify imports

---
+++ src/components/common/index.ts
@@ -0,0 +1,2 @@
+export { default as SectionHeader } from "./SectionHeader.vue";
+export { default as TrustBadge } from "./TrustBadge.vue";
---
+++ src/components/home/index.ts
@@ -0,0 +1,3 @@
+export { default as FlashSales } from "./FlashSales.vue";
+export { default as ExploreProducts } from "./ExploreProducts.vue";
+export { default as TrustBadges } from "./TrustBadges.vue";
