0fc2c83e6dddceab8114b87cb71beec81e854e6f
Salma Waleed <salmawaleed2030@outlook.com>
2026-04-12 12:25:59 +0200

    [fix]:[ExploreProducts]: added missing btn "view all products"

--- src/components/home/ExploreProducts.vue
+++ src/components/home/ExploreProducts.vue
@@ -4,21 +4,34 @@
       <SectionHeader subtitle="Our Products" title="Explore Our Products" />
     </div>
     <ProductList :products="products" />
+    <div class="explore-products__view-all">
+      <BaseButton text="View All Products" variant="primary" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ExploreProducts',
-    components: {
-        SectionHeader: () => import("@/components/common/SectionHeader.vue"),
-        ProductList: () => import("@/components/product/ProductList.vue"),
-    },
+  name: "ExploreProducts",
+  components: {
+    SectionHeader: () => import("@/components/common/SectionHeader.vue"),
+    ProductList: () => import("@/components/product/ProductList.vue"),
+    BaseButton: () => import("@/components/ui/BaseButton.vue"),
+  },
   props: {
     products: {
       type: Array,
       required: true,
     },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.explore-products {
+  &__view-all {
+    display: flex;
+    justify-content: center;
+    margin-top: 35px;
   }
 }
-</script>
+</style>
