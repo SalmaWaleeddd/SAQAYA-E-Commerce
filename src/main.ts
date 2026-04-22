4c06f7074a1d895a4bc876304a0c89cfc1372311
Salma Waleed <salmawaleed2030@outlook.com>
2026-04-22 16:09:39 +0200

    [feat]:[app]: migrate main.ts to Vue 3 and Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { createPinia } from "pinia";
 
-Vue.config.productionTip = false;
+const app = createApp(App);
+const pinia = createPinia();
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+app.use(router);
+app.use(pinia);
+app.mount("#app");
